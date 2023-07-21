a70492ea41c9eb9196840528b274da08905d70fe
zhw <zhw@buyelec.net>
2023-07-21 17:28:24 +0800

    新增一些基础配置

---
+++ src/components/layouts/DataHubHeader.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { } from "vue";
+</script>
+
+<template>
+    <div class="container header-container">
+        <div class="row">
+            <div class="stain col-2"></div>
+            <div class="stain col-8"></div>
+            <div class="stain col-2"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../../styles/global/index.scss';
+
+.header-container {
+    border-bottom: 1px solid $line-color;
+}
+
+.stain {
+    height: 50px;
+    border: 5px solid black;
+    background-color: rgb(51, 255, 153, 0.2) !important;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './styles/element/replace.scss'
+import ElementPlus from 'element-plus'
+import '@/styles/index.scss'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
